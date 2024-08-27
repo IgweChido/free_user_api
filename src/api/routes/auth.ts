28a41f1458ba7dc7653a6bf66f4ca3b2e5991a39
Igwe Chidera <igwechiderax@yahoo.com>
2024-08-27 19:14:36 +0100

    remove unecessary files

--- src/api/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const route = Router();
-export default (app: Router) => {
-  // initialize the main route
-  app.use("/auth", route);
-  // get all auth router
-};
