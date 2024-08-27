750c7b89ab5a85a261faf57cdf002f2c19e6644d
Igwe Chidera <igwechiderax@yahoo.com>
2024-08-27 22:26:50 +0100

    adding build script

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 const dotenv = require("dotenv").config();
 
 const envfound = dotenv;
-if (envfound.error) {
-  // this error should crash the whole process
+// if (envfound.error) {
+//   // this error should crash the whole process
 
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 export default {
   /* 
   Your favourite port
